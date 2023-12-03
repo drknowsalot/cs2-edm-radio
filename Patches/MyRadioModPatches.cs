c64d12d38ff3ef0ec385655abfe4f86d034605f4
AlphaGaming7780 <69048433+AlphaGaming7780@users.noreply.github.com>
2023-12-03 18:23:18 +0100

    Initial commit

---
+++ Patches/MyRadioModPatches.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Reflection;
+using Game.SceneFlow;
+using Game.UI;
+using HarmonyLib;
+using UnityEngine;
+
+namespace MyRadioMod.Patches
+{
+	[HarmonyPatch(typeof(GameManager), "InitializeThumbnails")]
+	internal class GameManager_InitializeThumbnails
+	{	
+		static readonly string IconsResourceKey = $"{MyPluginInfo.PLUGIN_NAME.ToLower()}";
+
+		public static readonly string COUIBaseLocation = $"coui://{IconsResourceKey}";
+
+		static void Prefix(GameManager __instance)
+		{		
+
+			Directory.CreateDirectory(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "CustomRadio"));
+
+			string resources = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "resources");
+
+			if(!Directory.Exists(resources)) {
+				Directory.CreateDirectory(resources);
+				File.Move(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "DefaultIcon.svg"), Path.Combine(resources , "DefaultIcon.svg"));
+			}
+
+			var gameUIResourceHandler = (GameUIResourceHandler)GameManager.instance.userInterface.view.uiSystem.resourceHandler;
+			
+			if (gameUIResourceHandler == null)
+			{
+				Debug.LogError("Failed retrieving GameManager's GameUIResourceHandler instance, exiting.");
+				return;
+			}
+			
+			gameUIResourceHandler.HostLocationsMap.Add(
+				IconsResourceKey,
+				[
+					Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+				]
+			);
+
+			ExtendedRadio.ExtendedRadio.CallOnRadioLoad += MyRadioMod.OnRadioLoad;
+
+		}
+	}
+}
---
+++ mod/mod.cs
@@ -0,0 +1,18 @@
+
+using System.IO;
+using static Game.Audio.Radio.Radio;
+
+namespace MyRadioMod 
+{
+	public class MyRadioMod 
+	{	
+		private static string radioDirec = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "CustomRadio");
+		public static void OnRadioLoad() {
+			RadioNetwork radioNetwork = ExtendedRadio.ExtendedRadio.JsonToRadioNetwork(radioDirec+"\\TestNetwork");
+			RadioChannel radioChannel = ExtendedRadio.ExtendedRadio.JsonToRadio(radioDirec+"\\TestNetwork\\JsonAndPath", radioNetwork.name);
+
+			ExtendedRadio.ExtendedRadio.AddRadioNetworkToTheGame(radioNetwork);
+			ExtendedRadio.ExtendedRadio.AddRadioChannelToTheGame(radioChannel, radioDirec+"\\TestNetwork\\JsonAndPath");
+		}
+	}
+}
